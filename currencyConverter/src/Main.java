b18b796f5eff39bef4934e8fa5aa718b72b1e2ff
Pasindu Ransika <v.pasinduransika@gmail.com>
2025-09-01 23:34:16 +0530

    all logics are added

--- currencyConverter/src/Main.java
+++ currencyConverter/src/Main.java
@@ -1,10 +1,34 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println(" -- Currency Converter --");
-        
+        Scanner input = new Scanner(System.in);
+        int lktous =300;
+        int total;
+        System.out.println("          -- Currency Converter --");
+        System.out.println(" ");
+        System.out.print("Chose Your Currency : ( US - 1 | LKR - 2 ) -- ");
+        int mainCur = input.nextInt();
+        System.out.print("Enter The Amount ( $ | LKR ) -  ");
 
+        int amount = input.nextInt();
+        System.out.print("To What Currency : ( US - 1 | LKR - 2 ) -- " );
+        int secCur = input.nextInt();
 
+        if (mainCur == 1 && secCur == 2){
+            total = lktous * amount;
+            System.out.println("\n               You Have LKR "+total);
+        }
+        else if (mainCur == 2 && secCur == 1){
+            total = amount / lktous ;
+            System.out.println("You Have - $ "+total);
+        }
+        else{
+            System.out.println("\n           ** ERROR !  TRY AGAIN **");
+        }
 
+        System.out.println("\n              -- Thank You ! -- ");
+        System.out.println("\n         -- MADE BY PASINDU RANSIKA -- ");
+        input.close();
 
         }
     }
